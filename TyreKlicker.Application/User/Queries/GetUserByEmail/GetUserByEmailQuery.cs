78ced2bd6194b2b747ad264ec9422b489d53c2ce
Ryan Stuart <ryan_7229@hotmail.co.uk>
2018-11-13 20:52:47 +0000

    namespace change

--- TyreKlicker.Application/User/Queries/GetUserByEmail/GetUserByEmailQuery.cs
+++ TyreKlicker.Application/User/Queries/GetUserByEmail/GetUserByEmailQuery.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace TyreKlicker.Application.User.Queries.GetUserByAccountId
+namespace TyreKlicker.Application.User.Queries.GetUserByEmail
 {
     public class GetUserByEmailQuery : IRequest<UserViewModel>
     {
