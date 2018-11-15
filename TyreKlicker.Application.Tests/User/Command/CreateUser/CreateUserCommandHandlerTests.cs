bd0754049b7e5673f655943912caa942d1e1300b
Ryan Stuart <ryan_7229@hotmail.co.uk>
2018-11-15 19:50:31 +0000

    remove redudnant test

--- TyreKlicker.Application.Tests/User/Command/CreateUser/CreateUserCommandHandlerTests.cs
+++ TyreKlicker.Application.Tests/User/Command/CreateUser/CreateUserCommandHandlerTests.cs
@@ -10,12 +10,10 @@ using Xunit;
 namespace TyreKlicker.Application.Tests.User.Command.CreateUser
 {
     [Collection("QueryCollection")]
-
     public class CreateUserCommandHandlerTests
     {
         private readonly TyreKlickerDbContext _context;
         private readonly string _email = "rsrs@hot.co.uk";
-        //private readonly string _validUserEmail = "ryan@email.co.uk";
 
         public CreateUserCommandHandlerTests(QueryTestFixture fixture)
         {
@@ -26,36 +24,13 @@ namespace TyreKlicker.Application.Tests.User.Command.CreateUser
         public async Task CreateUserCommand_UserIsCreated_ReturnsUserModel()
         {
             var sut = new CreateUserCommandHandler(_context);
+            var userCountBeforeAct = _context.User.ToList().Count;
 
-            await sut.Handle(new CreateUserCommand
-            {
-                FirstName="srs",
-                LastName = "steven",
-                PhoneNumber ="4855" ,
-                Email = _email
-            }, CancellationToken.None);
+            await sut.Handle(new CreateUserCommand { Email = _email }, CancellationToken.None);
             var user = _context.User.FirstOrDefault(e => e.Email == _email);
-            
 
+            _context.User.ToList().Count.ShouldBe(userCountBeforeAct + 1);
             user.ShouldBeOfType<Domain.Entities.User>();
-        }
-
-        [Fact]
-        public async Task CreateUserCommand_UserIsNotCreated_ReturnsUserModel()
-        {
-            var sut = new CreateUserCommandHandler(_context);
-
-            await sut.Handle(new CreateUserCommand
-            {
-                FirstName = "",
-                LastName = "",
-                PhoneNumber = "",
-                Email = ""
-            }, CancellationToken.None);
-            var user = _context.User.FirstOrDefault(e => e.Email == _email);
-
-
-            user.ShouldBeNull();
         }
     }
 }
---
+++ TyreKlicker.Application.Tests/User/Command/CreateUser/CreateUserCommandValidatorTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TyreKlicker.Application.Tests.User.Command.CreateUser
+{
+    class CreateUserCommandValidatorTests
+    {
+    }
+}
