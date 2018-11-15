163502b8f7561f02fee3c591fd72998a0f4b5b78
SyanRtuart <Ryan_7229@hotmail.co.uk>
2018-11-15 16:53:49 +0000

    added CreateUserCommand test

--- TyreKlicker.Application.Tests/User/Command/CreateUser/CreateUserCommandHandlerTests.cs
+++ TyreKlicker.Application.Tests/User/Command/CreateUser/CreateUserCommandHandlerTests.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using Shouldly;
+﻿using Shouldly;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using TyreKlicker.Application.Exceptions;
 using TyreKlicker.Application.Tests.Infrastructure;
-using TyreKlicker.Application.User.Queries.GetUserByEmail;
+using TyreKlicker.Application.User.Command.CreateUser;
 using TyreKlicker.Persistence;
 using Xunit;
 
@@ -15,12 +14,48 @@ namespace TyreKlicker.Application.Tests.User.Command.CreateUser
     public class CreateUserCommandHandlerTests
     {
         private readonly TyreKlickerDbContext _context;
+        private readonly string _email = "rsrs@hot.co.uk";
+        //private readonly string _validUserEmail = "ryan@email.co.uk";
 
-
-        public GetUserByEmailQueryHandlerTests(QueryTestFixture fixture)
+        public CreateUserCommandHandlerTests(QueryTestFixture fixture)
         {
             _context = fixture.Context;
         }
 
+        [Fact]
+        public async Task CreateUserCommand_UserIsCreated_ReturnsUserModel()
+        {
+            var sut = new CreateUserCommandHandler(_context);
+
+            await sut.Handle(new CreateUserCommand
+            {
+                FirstName="srs",
+                LastName = "steven",
+                PhoneNumber ="4855" ,
+                Email = _email
+            }, CancellationToken.None);
+            var user = _context.User.FirstOrDefault(e => e.Email == _email);
+            
+
+            user.ShouldBeOfType<Domain.Entities.User>();
+        }
+
+        [Fact]
+        public async Task CreateUserCommand_UserIsNotCreated_ReturnsUserModel()
+        {
+            var sut = new CreateUserCommandHandler(_context);
+
+            await sut.Handle(new CreateUserCommand
+            {
+                FirstName = "",
+                LastName = "",
+                PhoneNumber = "",
+                Email = ""
+            }, CancellationToken.None);
+            var user = _context.User.FirstOrDefault(e => e.Email == _email);
+
+
+            user.ShouldBeNull();
+        }
     }
 }
