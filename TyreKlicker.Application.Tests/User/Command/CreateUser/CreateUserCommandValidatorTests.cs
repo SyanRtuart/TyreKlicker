2ba64b63e489f3dbe293da8ac0ba459340d3ab4b
Ryan Stuart <ryan_7229@hotmail.co.uk>
2018-11-15 20:40:28 +0000

    Added CreateUserCommandValidator tests

--- TyreKlicker.Application.Tests/User/Command/CreateUser/CreateUserCommandValidatorTests.cs
+++ TyreKlicker.Application.Tests/User/Command/CreateUser/CreateUserCommandValidatorTests.cs
@@ -1,10 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FluentValidation;
+using FluentValidation.TestHelper;
+using TyreKlicker.Application.User.Command.CreateUser;
+using Xunit;
 
 namespace TyreKlicker.Application.Tests.User.Command.CreateUser
 {
-    class CreateUserCommandValidatorTests
+    public class CreateUserCommandValidatorTests
     {
+        private CreateUserCommandValidator _validator;
+
+        public CreateUserCommandValidatorTests()
+        {
+            _validator = new CreateUserCommandValidator();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("a")]
+        [InlineData("MyNameIsVeryLongAndIShouldReturnAnError")]
+        public void CreateUserCommandValidator_FirstName_ReturnsValidationErrors(string firstName)
+        {
+            _validator.ShouldHaveValidationErrorFor(u => u.FirstName, firstName);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("a")]
+        [InlineData("MyNameIsVeryLongAndIShouldReturnAnError")]
+        public void CreateUserCommandValidator_LastName_ReturnsValidationErrors(string lastName)
+        {
+            _validator.ShouldHaveValidationErrorFor(u => u.LastName, lastName);
+        }
+
+        [Fact]
+        public void CreateUserCommandValidator_PhoneNumberIsEmpty_ReturnsValidationErrors()
+        {
+            _validator.ShouldHaveValidationErrorFor(u => u.PhoneNumber, "");
+        }
     }
 }
