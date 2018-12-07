f0f174dc5b6fdc867bde64c170be1188849b46b6
SyanRtuart <Ryan_7229@hotmail.co.uk>
2018-12-07 15:40:37 +0000

    added Already accepted exception

---
+++ TyreKlicker.Application/Exceptions/AlreadyAcceptedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TyreKlicker.Application.Exceptions
+{
+    public class AlreadyAcceptedException : Exception
+    {
+        public AlreadyAcceptedException(string name, object key)
+            : base($"Entity \"{name}\" ({key}) is already accepted.")
+        {
+        }
+    }
+}
