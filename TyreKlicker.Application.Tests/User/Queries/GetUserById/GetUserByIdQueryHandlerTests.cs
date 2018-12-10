fb9d02aebfc5e2a4fb36a78e1ad6c75ee65153ff
Ryan Stuart <ryan_7229@hotmail.co.uk>
2018-12-10 20:36:50 +0000

    added Get user by id tests

---
+++ TyreKlicker.Application.Tests/User/Queries/GetUserById/GetUserByIdQueryHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using Shouldly;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using TyreKlicker.Application.Exceptions;
+using TyreKlicker.Application.Tests.Infrastructure;
+using TyreKlicker.Application.User.Queries.GetUserById;
+using TyreKlicker.Persistence;
+using Xunit;
+
+namespace TyreKlicker.Application.Tests.User.Queries.GetUserById
+{
+    [Collection("QueryCollection")]
+    public class GetUserByIdQueryHandlerTests
+    {
+        private readonly TyreKlickerDbContext _context;
+        private readonly Guid _userInDb = Guid.Parse("2220d661-6a96-4537-a896-5014374d39f5");
+
+        public GetUserByIdQueryHandlerTests(QueryTestFixture fixture)
+        {
+            _context = fixture.Context;
+        }
+
+        [Fact]
+        public async Task GetUserById_UserExists_ReturnsUserViewModel()
+        {
+            var sut = new GetUserByIdQueryHandler(_context);
+
+            var result = await sut.Handle(new GetUserByIdQuery { Id = _userInDb }, CancellationToken.None);
+
+            result.ShouldBeOfType<UserViewModel>();
+            result.Id.ShouldBe(_userInDb);
+        }
+
+        [Fact]
+        public async Task GetUserById_UserDoesNotExist_ThrowsNotFoundException()
+        {
+            var sut = new GetUserByIdQueryHandler(_context);
+
+            var result = await sut.Handle(new GetUserByIdQuery { Id = Guid.NewGuid() }, CancellationToken.None)
+
+            .ShouldThrowAsync<NotFoundException>();
+        }
+    }
+}
