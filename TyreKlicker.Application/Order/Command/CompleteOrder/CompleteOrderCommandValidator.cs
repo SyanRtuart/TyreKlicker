c8c7025fe0d9076fb7ab417bb6db9bf6835b2e63
SyanRtuart <Ryan_7229@hotmail.co.uk>
2018-12-10 16:06:40 +0000

    Added Complete Order Command Validator

---
+++ TyreKlicker.Application/Order/Command/CompleteOrder/CompleteOrderCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+using TyreKlicker.Application.Infrastructure;
+
+
+namespace TyreKlicker.Application.Order.Command.CompleteOrder
+{
+    public class CompleteOrderCommandValidator : AbstractValidator<CompleteOrderCommand>
+    {
+        public CompleteOrderCommandValidator()
+        {
+            RuleFor(o => o.OrderId)
+                .NotNull()
+                .NotEmpty()
+                .SetValidator(new GuidValidator());
+
+            RuleFor(o => o.Complete)
+                .NotEmpty()
+                .NotNull();
+
+
+        }
+    }
+}
