b7a611387e986cfb9ca2a101d7c2ec7cd9124b8e
Ryan Stuart <ryan_7229@hotmail.co.uk>
2019-04-03 20:59:58 +0100

    added get address and tests

---
+++ TyreKlicker.Application/Address/Query/GetAddress/GetAddressQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TyreKlicker.Application.Exceptions;
+using TyreKlicker.Persistence;
+
+namespace TyreKlicker.Application.Address.Query.GetAddress
+{
+    public class GetAddressQueryHandler : IRequestHandler<GetAddressQuery, AddressDto>
+    {
+        private readonly TyreKlickerDbContext _context;
+
+        public GetAddressQueryHandler(TyreKlickerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<AddressDto> Handle(GetAddressQuery request, CancellationToken cancellationToken)
+        {
+            var addressInDb = await _context.Address
+                .Where(a => a.Id == request.Id)
+                .Select(AddressDto.Projection)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (addressInDb == null) throw new NotFoundException(nameof(addressInDb), request.Id);
+
+            return addressInDb;
+        }
+    }
+}
